e445558c83d14f46c91c20b090b87f4d38b1afdf
Franc <francisco2do1994@gmail.com>
2025-12-04 02:26:26 -0300

    Api integrada para el registro de un vehiculo

---
+++ TallerMecanico.Web/Models/FipeModels.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TallerMecanico.Web.Models
+{
+    // CLASE 1: Para las MARCAS (El código viene como STRING "59")
+    public class FipeItem
+    {
+        [JsonPropertyName("nome")]
+        public string Nombre { get; set; }
+
+        [JsonPropertyName("codigo")]
+        public string Codigo { get; set; }
+    }
+
+    // CLASE 2: Para los MODELOS (El código viene como NÚMERO 5585)
+    public class FipeModelo
+    {
+        [JsonPropertyName("nome")]
+        public string Nombre { get; set; }
+
+        [JsonPropertyName("codigo")]
+        public long Codigo { get; set; } // Usamos 'long' o 'int' porque es un número
+    }
+
+    // CLASE 3: La respuesta de la API que trae la lista de modelos
+    public class FipeModeloResponse
+    {
+        [JsonPropertyName("modelos")]
+        public List<FipeModelo> Modelos { get; set; } // Ahora usa la clase nueva
+
+        [JsonPropertyName("anos")]
+        public List<FipeItem> Anios { get; set; }
+    }
+}
--- TallerMecanico.Web/Program.cs
+++ TallerMecanico.Web/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection; // <--- AGREGA ESTA LÍNEA OBLIGATORIAMENTE
+using System;
+using System.Net.Http;
 
 namespace TallerMecanico.Web
 {
@@ -12,7 +15,12 @@ namespace TallerMecanico.Web
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            
+            builder.Services.AddHttpClient("FipeApi", client =>
+            {
+                // Asegúrate de que esta sea la URL base correcta de la API que vas a usar
+                client.BaseAddress = new Uri("https://parallelum.com.br/fipe/api/v1/");
+            });
             await builder.Build().RunAsync();
         }
     }
