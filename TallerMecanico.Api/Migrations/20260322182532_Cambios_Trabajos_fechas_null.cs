9f8e2f9306c090988bbe0bd5721b46e12741a22c
Francisco <francisco2do1994@gmail.com>
2026-03-22 16:35:40 -0300

    Avances con el frontend de Trabajos, ahora anda pero faltan campos en el formulario

---
+++ TallerMecanico.Api/Migrations/20260322182532_Cambios_Trabajos_fechas_null.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TallerMecanico.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cambios_Trabajos_fechas_null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trabajos_Vehiculos_vehiculoId",
+                table: "Trabajos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "vehiculoId",
+                table: "Trabajos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fechaPago",
+                table: "Trabajos",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fechaFinalizacion",
+                table: "Trabajos",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trabajos_Vehiculos_vehiculoId",
+                table: "Trabajos",
+                column: "vehiculoId",
+                principalTable: "Vehiculos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trabajos_Vehiculos_vehiculoId",
+                table: "Trabajos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "vehiculoId",
+                table: "Trabajos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fechaPago",
+                table: "Trabajos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fechaFinalizacion",
+                table: "Trabajos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trabajos_Vehiculos_vehiculoId",
+                table: "Trabajos",
+                column: "vehiculoId",
+                principalTable: "Vehiculos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- TallerMecanico.Api/Models/Trabajo.cs
+++ TallerMecanico.Api/Models/Trabajo.cs
@@ -1,18 +1,20 @@
 ﻿namespace TallerMecanico.Api.Models
 {
+    using System.Text.Json.Serialization;
     public class Trabajo
     {
         public int id { get; set; }
         public DateTime fechaAsignacion { get; set; }
-        public DateTime fechaFinalizacion { get; set; }
+        public DateTime? fechaFinalizacion { get; set; }
 
-        public DateTime fechaPago { get; set; }
+        public DateTime? fechaPago { get; set; }
         public float monto { get; set; }
         public string descripcionArreglo { get; set; }
         public EstadoTrabajo estado { get; set; } = new EstadoTrabajo();
 
         //[Required]
-        public Vehiculo vehiculo { get; set; }
+        [JsonIgnore]
+        public Vehiculo? vehiculo { get; set; }
     }
 
 
